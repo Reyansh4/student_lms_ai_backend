111b915489c691e1930a0decd7e524ce0cab3a0c
Uday Sambyal <udaysambyal@Udays-MacBook-Air.local>
2025-05-17 02:01:48 +0530

    removed few checks

--- app/core/config.py
+++ app/core/config.py
@@ -15,6 +15,7 @@ class Settings(BaseSettings):
     JWT_SECRET_KEY: str = secrets.token_urlsafe(32)
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRY_TIME: int = 60  # Default to 60 minutes
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
     
     # Database settings
